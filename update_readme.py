80dfeeb53dcbec19e8dd7074153a175508bde847
Gaurab Kumar <progaurab@gmail.com>
2024-08-24 14:31:13 +0530

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -6,13 +6,13 @@ with open('blog_posts.txt', 'r') as f:
 with open('README.md', 'r') as f:
     readme_content = f.read()
 
-new_blog_posts_section = "## Latest blog on https://instilllearning.com\n\n" 
+new_blog_posts_section = '## Latest blog on https://instilllearning.com\n\n' 
 for url, title, image_url in blog_posts:
     if image_url:
         new_blog_posts_section += f'<a href="{url}"><img src="{image_url}" alt="{title}" width="100" align="left" hspace="10" vspace="10"></a>'
-    new_blog_posts_section += f"<p>**[{title}]({url})**</p>\n"
+    new_blog_posts_section += f'<p>**[{title}]({url})**</p>\n'
 
-updated_readme_content = re.sub(r'## Recent Blog Posts.*|## Latest blog on https://instilllearning.com.*', new_blog_posts_section, readme_content, flags=re.DOTALL)
+updated_readme_content = re.sub(r'## Latest blog on https://instilllearning.com.*', new_blog_posts_section, readme_content, flags=re.DOTALL)
 
 with open('README.md', 'w') as f:
     f.write(updated_readme_content)
